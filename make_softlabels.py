3fd46a46267f351e2c5e7b4064679d11b6ce69bb
raghavian <raghav@di.ku.dk>
2024-11-01 14:19:08 +0100

    Update softlabels

---
+++ make_softlabels.py
@@ -0,0 +1,20 @@
+import torch
+import numpy as np
+from scipy.ndimage import gaussian_filter
+from skimage import io
+import pdb
+
+x, y = torch.load('data/facemap_224.pt')
+h,w = x[0].shape[1], x[1].shape[2]
+sigma = 3
+softlabels = torch.zeros(x.shape)
+for i in range(len(y)):
+    mask = np.zeros((h,w))
+    y_i = y[i][~np.isnan(y[i])]
+    idx_x, idx_y = y_i[1::2].astype(int), y_i[::2].astype(int)
+    mask[idx_x,idx_y] = 1
+    label = gaussian_filter(mask,sigma=sigma)
+    softlabels[i,0] = torch.Tensor(label)
+
+pdb.set_trace()
+torch.save((x,y,softlabels),'data/facemap_softlabels.pt')
